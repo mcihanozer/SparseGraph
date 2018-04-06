194d9bf299e65ddeb64a8c7b5257f0c6b15904b9
M. Cihan Ozer <ozer.mc@gmail.com>
2018-04-06 18:03:11 +0300

    Create ErrorPrinter.cs

---
+++ Helpers/ErrorPrinter.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public static class ErrorPrinter
+{
+    public static void PrintError(string className, string functionName, string errorDetails = "", string errorBy = "")
+    { 
+        Debug.LogFormat(
+                            "<color=#ff0000ff><b>ERROR</b></color> IN CLASS <color=#ffa500ff>{0}</color>" +
+                            "IN METHOD <color=#d69848>{1}</color>" +
+                            "ERROR DETAILS: <color=#0000ffff>{2}</color>" +
+                            " ERROR BY: {3}",
+                            className, functionName, errorDetails, errorBy
+                        );
+    }
+}
