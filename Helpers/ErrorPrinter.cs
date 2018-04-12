56e32e5a135a43c49bb2d0181c077a9931a9ae29
M. Cihan Ozer <ozer.mc@gmail.com>
2018-04-12 17:32:42 +0300

    Update ErrorPrinter.cs

--- Helpers/ErrorPrinter.cs
+++ Helpers/ErrorPrinter.cs
@@ -8,8 +8,8 @@ public static class ErrorPrinter
     { 
         Debug.LogFormat(
                             "<color=#ff0000ff><b>ERROR</b></color> IN CLASS <color=#ffa500ff>{0}</color>" +
-                            "IN METHOD <color=#d69848>{1}</color>" +
-                            "ERROR DETAILS: <color=#0000ffff>{2}</color>" +
+                            " IN METHOD <color=#d69848>{1}</color>" +
+                            " ERROR DETAILS: <color=#0000ffff>{2}</color>" +
                             " ERROR BY: {3}",
                             className, functionName, errorDetails, errorBy
                         );
